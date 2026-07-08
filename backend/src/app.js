c6147777866a6acee1ffc74d9cc8c6b194dcc1c5
Kapil Singh <kapilsinghchandrawat@gmail.com>
2026-07-08 16:38:41 +0530

    final fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,30 +1,39 @@
 import express from "express";
-import {createServer} from "node:http";
-import { Server } from "socket.io";
-import mongoose, { connections } from "mongoose";
-import connectToSocket from "./controllers/socket-manager.js";
-import cors from "cors";
-import userRoutes from "./routes/users-routes.js"
-import router from "./routes/users-routes.js";
-const app=express();
-const server=createServer(app);
-const io=connectToSocket(server)
-app.set("port",(process.env.port||8000));
-app.use(cors());
-app.use(express.json({limit:"40kb"}));
-app.use(express.urlencoded({limit:"40kb",extended:true}))
-app.use("/api/v1/users",userRoutes);
-// app.use("/api/v2/users",newuserroutes)
- app.get("/home",(req,res)=>{
-    return res.json({"hello":"world"});
+import { createServer } from "node:http";
 
- });
- const start=async()=>{
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socket-manager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users-routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
     app.set("mongo_user")
-    const connectiondb=await mongoose.connect("mongodb+srv://kapilsinghchandrawat_db_user:fonLB11Uo18UO7tP@cluster0.2ri15bf.mongodb.net/")
-    console.log(`mongo connecteddb host:${connectiondb.connection.host}`)
-    app.listen(app.get("port"),()=>{
-        console.log("listening on port 8000");
+    const connectionDb = await mongoose.connect("mongodb+srv://kapilsinghchandrawat_db_user:fonLB11Uo18UO7tP@cluster0.2ri15bf.mongodb.net/")
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENIN ON PORT 8000")
     });
- }
+
+
+
+}
+
+
+
 start();
--- backend/src/controllers/socket-manager.js
+++ backend/src/controllers/socket-manager.js
@@ -1,86 +1,120 @@
-import { Server } from "socket.io";
-let connection = {};
-let message = {};
-let timeOnline = {};
+import { Server } from "socket.io"
 
-const connectToSocket = (server) => {
-    const io = new Server(server,{
-        cors:{
-            origin:"*",
-            methods:["get","post"],
-            allowedheaders:["*"],
-            credentials:true
+
+let connections = {}
+let messages = {}
+let timeOnline = {}
+
+export const connectToSocket = (server) => {
+    const io = new Server(server, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"],
+            allowedHeaders: ["*"],
+            credentials: true
         }
     });
-    io.on("connection", (socket) => {
-        socket.on("join-call", (path) => {
-            if (connection[path] === undefined) {
-                connection[path].push(socket.id);
-                timeOnline[socket.id] = new Date();
-                // connection[path].foreach
-                for (let a = 0; a < connection[path].length; i++) {
-                    io.on(connection[path][a]).emit(user - joined, socket.id, connection[path])
-                }
-                if (message[path] === undefined) {
-                    for (let a = 0; a < message[path].length; ++a) {
-                        io.to(socket.id).emit("chat-message", message[path][a]['data'],
 
-                            message[path][a]['sender'], message[path][a]['socket-id-sender']
-                        )
-                    }
+
+    io.on("connection", (socket) => {
+
+        console.log("SOMETHING CONNECTED")
+
+        socket.on("join-call", (path) => {
+
+            if (connections[path] === undefined) {
+                connections[path] = []
+            }
+            connections[path].push(socket.id)
+
+            timeOnline[socket.id] = new Date();
+
+            // connections[path].forEach(elem => {
+            //     io.to(elem)
+            // })
+
+            for (let a = 0; a < connections[path].length; a++) {
+                io.to(connections[path][a]).emit("user-joined", socket.id, connections[path])
+            }
+
+            if (messages[path] !== undefined) {
+                for (let a = 0; a < messages[path].length; ++a) {
+                    io.to(socket.id).emit("chat-message", messages[path][a]['data'],
+                        messages[path][a]['sender'], messages[path][a]['socket-id-sender'])
                 }
             }
 
         })
-        socket.on("signal", (toId, messege) => {
-            io.on(toId).emit("signal", socket.id, messege);
 
-
+        socket.on("signal", (toId, message) => {
+            io.to(toId).emit("signal", socket.id, message);
         })
+
         socket.on("chat-message", (data, sender) => {
-            const [matchingroom, found] = Object.entries(connection)
+
+            const [matchingRoom, found] = Object.entries(connections)
                 .reduce(([room, isFound], [roomKey, roomValue]) => {
+
+
                     if (!isFound && roomValue.includes(socket.id)) {
                         return [roomKey, true];
                     }
+
                     return [room, isFound];
+
                 }, ['', false]);
+
             if (found === true) {
-                if (message[matchingroom] === undefined) {
-                    message[matchingroom] = []
+                if (messages[matchingRoom] === undefined) {
+                    messages[matchingRoom] = []
                 }
-                message[matchingroom].push({ 'sender': sender, "data": data, "socket-id-sender": socket.id })
-                console.log("message", matchingroom, ":", sender, data)
-                connection[matchingroom].forEach((elem) => {
+
+                messages[matchingRoom].push({ 'sender': sender, "data": data, "socket-id-sender": socket.id })
+                console.log("message", matchingRoom, ":", sender, data)
+
+                connections[matchingRoom].forEach((elem) => {
                     io.to(elem).emit("chat-message", data, sender, socket.id)
                 })
             }
-        }
 
-        )
-
+        })
 
         socket.on("disconnect", () => {
-            var difftime = Math.abs(timeOnline[socket.id] - new Date())
+
+            var diffTime = Math.abs(timeOnline[socket.id] - new Date())
+
             var key
-            for (const [k, v] of JSON.parse(JSON.stringify(Object.entries(connection)))) {
+
+            for (const [k, v] of JSON.parse(JSON.stringify(Object.entries(connections)))) {
+
                 for (let a = 0; a < v.length; ++a) {
                     if (v[a] === socket.id) {
                         key = k
-                        for (let a = 0; a < connection[key].length; ++a) {
-                            io.to(connection[key][a]).emit('user-left', socket.id)
+
+                        for (let a = 0; a < connections[key].length; ++a) {
+                            io.to(connections[key][a]).emit('user-left', socket.id)
                         }
-                        var index = connection[key].indexoF(socket.id)
-                        connection[key].splice(index, 1)
-                        if (connection[key].length === 0) {
-                            delete connection[key]
+
+                        var index = connections[key].indexOf(socket.id)
+
+                        connections[key].splice(index, 1)
+
+
+                        if (connections[key].length === 0) {
+                            delete connections[key]
                         }
                     }
                 }
+
             }
+
+
         })
 
+
     })
+
+
     return io;
 }
-export default connectToSocket;
+
